8057ba8f06a089b8a1e7066434bd59a3a0792901
DESKTOP-SF5NH44\EQ01 <luis.rendon@bpt.com.co>
2022-03-15 22:22:13 -0500

    Se implementan los servicios en el backend para crear usuarios y verificar sus datos para el login

--- src/main/java/co/edu/udea/infocovid/App.java
+++ src/main/java/co/edu/udea/infocovid/App.java
@@ -2,10 +2,8 @@ package co.edu.udea.infocovid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
---
+++ src/main/java/co/edu/udea/infocovid/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+package co.edu.udea.infocovid.controller;
+
+import co.edu.udea.infocovid.facade.UsuarioFacade;
+import co.edu.udea.infocovid.mapper.UsuarioMapper;
+import co.edu.udea.infocovid.modelo.UsuarioDTO;
+import co.edu.udea.infocovid.util.Messages;
+import co.edu.udea.infocovid.util.StandardResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    private final UsuarioFacade usuarioFacade;
+    private final UsuarioMapper usuarioMapper;
+    private final Messages messages;
+
+    public UsuarioController(UsuarioFacade usuarioFacade, UsuarioMapper usuarioMapper, Messages messages) {
+        this.usuarioFacade = usuarioFacade;
+        this.usuarioMapper = usuarioMapper;
+        this.messages = messages;
+    }
+
+    @PostMapping()
+    @ApiOperation(value = "Permite crear un usuario", response = UsuarioDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Se guardó el usuario exitosamente"),
+            @ApiResponse(code = 400, message = "La petición es inválida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<StandardResponse<UsuarioDTO>> guardarUsuario(@RequestBody UsuarioDTO user) {
+        UsuarioDTO userDto = usuarioFacade.crearUsuario(user);
+        return ResponseEntity.ok(new StandardResponse<>(StandardResponse.StatusStandardResponse.OK, userDto));    }
+
+    @GetMapping("iniciar-sesion/{correo}/{contrasena}")
+    @ApiOperation(value = "Permite verificar la información del usuario para iniciar sesión", response = UsuarioDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Sesión iniciada correctamente"),
+            @ApiResponse(code = 400, message = "La petición es inválida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<StandardResponse<UsuarioDTO>> buscarUsuarioPorCorreo(@PathVariable String correo, @PathVariable String contrasena) {
+        UsuarioDTO usuarioDTO = usuarioFacade.verificarInformacionInicioSesion(correo, contrasena);
+        return ResponseEntity.ok(new StandardResponse<>(StandardResponse.StatusStandardResponse.OK, usuarioDTO));
+    }
+}
